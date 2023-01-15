1b64832d93c99ea1b6e25428390293c0a0cba651
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2023-01-15 23:52:54 +0100

    fix: pubsub 2 bq function

--- src/cloud_functions/pubsub_2_bq.py
+++ src/cloud_functions/pubsub_2_bq.py
@@ -1,2 +1,7 @@
-def pubsub_2_bq():
+def pubsub_2_bq(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
     print("Hello world")
