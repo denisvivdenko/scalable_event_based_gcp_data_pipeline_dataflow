50e4786caaad145f181e7f81c6f338753919ddfd
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2023-01-22 16:57:35 +0100

    feat: add sftp dag

---
+++ src/composer/dags/sftp2cloud_storage.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.sftp.operators.sftp import SFTPOperator
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime, timedelta
+
+
+dag = DAG("sftp_dag",
+    start_date=datetime.now(),
+    schedule_interval=None,
+    default_args={
+        "owner": "airflow",
+        "depends_on_past": False,
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=5)
+    }
+)
+
+
+put_file = SFTPOperator(
+    task_id="test_sftp",
+    ssh_conn_id="ssh_default",
+    local_filepath="/home/airflow/gcs/data/covid_data.csv",
+    remote_filepath="/home/denys/covid_data.csv",
+    operation="put",
+    create_intermediate_dirs=True,
+    dag=dag
+)
+
+
+start_pipeline = DummyOperator(task_id="start_pipeline", dag=dag)
+
+start_pipeline >> put_file
